c3fca313d67b09b0746311045f8ca0c701fc3214
Fabian Juren <fjuren@gmail.com>
2022-06-27 20:14:25 -0700

    Full name validation JS

--- app.js
+++ app.js
@@ -4,23 +4,27 @@
 const form = document.getElementsByTagName('form')[0];
 
 const fullName = document.getElementById('fName');
+const fullNameInput = document.querySelector('input#fName');
 const fullNameErrorMessage = document.querySelectorAll('span.error')[0];
 
 // Assesses validation criteria for full name field
 const fullNameError = () => {
     if (fullName.validity.valueMissing) {
-        fullNameErrorMessage.textContent = 'Please enter your first and last name';
+        fullNameErrorMessage.textContent = 'Please enter your first and last name. (E.g., John Smith)';
+    } else if (fullName.validity.patternMismatch) {
+        fullNameErrorMessage.textContent = 'Please enter your full name. (E.g., John Smith)';
+        console.log('pattern mismatch');
     } else {
-        console.log('you\'re fine');
+        console.log('do nothing');
     }
 };
 
-// calls fullNameError when validation fails
+// Full name field prompt. Called when submit btn pressed
 const fullNameErrorHandling = () => {
-    if (!fullName.validity.valid) {
+    if (!fullNameInput.validity.valid) {
         fullNameError();
     } else {
-        console.log('no error');
+        fullNameErrorMessage.textContent = '';
     }
 };
 
