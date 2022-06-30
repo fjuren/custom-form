1a36a17010cc51a3f46c3b6d196cbbb7a673490f
Fabian Juren <fjuren@gmail.com>
2022-06-29 23:11:55 -0700

    Add all based JS for rest of input fields

--- app.js
+++ app.js
@@ -9,6 +9,40 @@ const fullNameErrorMessage = document.querySelectorAll('span.error')[0];
 const dobInput = document.querySelector('input#dob');
 const dobErrorMessage = document.querySelectorAll('span.error')[1];
 
+const addressInput = document.querySelector('input#address');
+const addressErrorMessage = document.querySelectorAll('span.error')[2];
+
+const unitNumberInput = document.querySelector('input#unitNumber');
+const unitNumberErrorMessage = document.querySelectorAll('span.error')[3];
+
+const cityInput = document.querySelector('input#city');
+const cityErrorMessage = document.querySelectorAll('span.error')[4];
+
+const stateProvinceInput = document.querySelector('input#stateProvince');
+const stateProvinceErrorMessage = document.querySelectorAll('span.error')[5];
+
+const zipInput = document.querySelector('input#zipPostal');
+const zipErrorMessage = document.querySelectorAll('span.error')[6];
+
+const countryInput = document.querySelector('input#country');
+const countryErrorMessage = document.querySelectorAll('span.error')[7];
+
+const emailInput = document.querySelector('input#email');
+const emailErrorMessage = document.querySelectorAll('span.error')[8];
+
+const phoneInput = document.querySelector('input#phone');
+const phoneErrorMessage = document.querySelectorAll('span.error')[9];
+
+const usernameInput = document.querySelector('input#username');
+const usernameErrorMessage = document.querySelectorAll('span.error')[10];
+
+const password1Input = document.querySelector('input#password1');
+const password1ErrorMessage = document.querySelectorAll('span.error')[11];
+
+const password2Input = document.querySelector('input#password2');
+const password2ErrorMessage = document.querySelectorAll('span.error')[12];
+
+
 // -> String
 // Assesses validation criteria for full name field
 const fullNameError = () => {
@@ -49,9 +83,174 @@ const dobErrorHandling = () => {
         }
 };
 
+// Assesses validation criteria for address field
+const addressError = () => {
+
+};
+
+// address prompt. Called when submit btn pressed
+const addressErrorHandling = () => {
+    if (!addressInput.validity.valid) {
+        addressError();
+        } else {
+            addressErrorMessage.textContent = '';
+        }
+};
+
+// Assesses validation criteria for unit number field
+const unitNumberError = () => {
+
+};
+
+// unit number prompt. Called when submit btn pressed
+const unitNumberErrorHandling = () => {
+    if (!unitNumberInput.validity.valid) {
+        unitNumberError();
+        } else {
+            unitNumberErrorMessage.textContent = '';
+        }
+};
+
+// Assesses validation criteria for city field
+const cityError = () => {
+
+};
+
+// City prompt. Called when submit btn pressed
+const cityErrorHandling = () => {
+    if (!cityInput.validity.valid) {
+        cityError();
+        } else {
+            cityErrorMessage.textContent = '';
+        }
+};
+
+// Assesses validation criteria for state/provnice field
+const stateProvinceError = () => {
+
+};
+
+// state/province prompt. Called when submit btn pressed
+const stataProvinceErrorHandling = () => {
+    if (!stateProvinceInput.validity.valid) {
+        stateProvinceError();
+        } else {
+            stateProvinceErrorMessage.textContent = '';
+        }
+};
+
+// Assesses validation criteria for zip/postal code field
+const zipError = () => {
+
+};
+
+// zip/postal code prompt. Called when submit btn pressed
+const zipErrorHandling = () => {
+    if (!zipInput.validity.valid) {
+        zipError();
+        } else {
+            zipErrorMessage.textContent = '';
+        }
+};
+
+// Assesses validation criteria for country field
+const countryError = () => {
+
+};
+
+// country prompt. Called when submit btn pressed
+const countryErrorHandling = () => {
+    if (!countryInput.validity.valid) {
+        countryError();
+        } else {
+            countryErrorMessage.textContent = '';
+        }
+};
+
+// Assesses validation criteria for email field
+const emailError = () => {
+
+};
+
+// email prompt. Called when submit btn pressed
+const emailErrorHandling = () => {
+    if (!emailInput.validity.valid) {
+        emailError();
+        } else {
+            emailErrorMessage.textContent = '';
+        }
+};
+
+// Assesses validation criteria for phone field
+const phoneError = () => {
+
+};
+
+// phone prompt. Called when submit btn pressed
+const phoneErrorHandling = () => {
+    if (!phoneInput.validity.valid) {
+        phoneError();
+        } else {
+            phoneErrorMessage.textContent = '';
+        }
+};
+
+// Assesses validation criteria for username field
+const usernameError = () => {
+
+};
+
+// username prompt. Called when submit btn pressed
+const usernameErrorHandling = () => {
+    if (!usernameInput.validity.valid) {
+        usernameError();
+        } else {
+            usernameErrorMessage.textContent = '';
+        }
+};
+
+// Assesses validation criteria for password1 field
+const password1Error = () => {
+
+};
+
+// password1 prompt. Called when submit btn pressed
+const password1ErrorHandling = () => {
+    if (!password1Input.validity.valid) {
+        password1Error();
+        } else {
+            password1ErrorMessage.textContent = '';
+        }
+};
+
+// Assesses validation criteria for password2 field
+const password2Error = () => {
+
+};
+
+// password2 prompt. Called when submit btn pressed
+const password2ErrorHandling = () => {
+    if (!password2Input.validity.valid) {
+        password2Error();
+        } else {
+            password2ErrorMessage.textContent = '';
+        }
+};
+
 // Runs all field validations on form submit
 form.addEventListener('submit', (event) => {
     fullNameErrorHandling();
     dobErrorHandling();
+    addressErrorHandling();
+    unitNumberErrorHandling();
+    cityErrorHandling();
+    stataProvinceErrorHandling();
+    zipErrorHandling();
+    countryErrorHandling();
+    emailErrorHandling();
+    phoneErrorHandling();
+    usernameErrorHandling();
+    password1ErrorHandling();
+    password2ErrorHandling();
     event.preventDefault();
 });
