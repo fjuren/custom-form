c13d8112fc089c6931ecf740eb9d4ab4c7373831
Fabian Juren <fjuren@gmail.com>
2022-06-29 22:08:06 -0700

    Add validation and styling to full name and dob inputs

--- app.js
+++ app.js
@@ -3,22 +3,25 @@
 
 const form = document.getElementsByTagName('form')[0];
 
-const fullName = document.getElementById('fName');
 const fullNameInput = document.querySelector('input#fName');
 const fullNameErrorMessage = document.querySelectorAll('span.error')[0];
 
+const dobInput = document.querySelector('input#dob');
+const dobErrorMessage = document.querySelectorAll('span.error')[1];
+
+// -> String
 // Assesses validation criteria for full name field
 const fullNameError = () => {
-    if (fullName.validity.valueMissing) {
-        fullNameErrorMessage.textContent = 'Please enter your first and last name. (E.g., John Smith)';
-    } else if (fullName.validity.patternMismatch) {
-        fullNameErrorMessage.textContent = 'Please enter your full name. (E.g., John Smith)';
-        console.log('pattern mismatch');
+    if (fullNameInput.validity.valueMissing) {
+        fullNameErrorMessage.textContent = 'Please enter your first and last name';
+    } else if (fullNameInput.validity.patternMismatch) {
+        fullNameErrorMessage.textContent = 'Look at the example for proper format (E.g., John Smith)';
     } else {
-        console.log('do nothing');
+        // do nothing
     }
 };
 
+// -> String
 // Full name field prompt. Called when submit btn pressed
 const fullNameErrorHandling = () => {
     if (!fullNameInput.validity.valid) {
@@ -28,8 +31,27 @@ const fullNameErrorHandling = () => {
     }
 };
 
+// Assesses validation criteria for date of birth field
+const dobError = () => {
+    if (dobInput.validity.valueMissing) {
+        dobErrorMessage.textContent = 'Please provide your date of birth';
+    } else {
+        // do nothing
+    }
+};
+
+// Date of birth prompt. Called when submit btn pressed
+const dobErrorHandling = () => {
+    if (!dobInput.validity.valid) {
+        dobError();
+        } else {
+            dobErrorMessage.textContent = '';
+        }
+};
+
 // Runs all field validations on form submit
 form.addEventListener('submit', (event) => {
     fullNameErrorHandling();
+    dobErrorHandling();
     event.preventDefault();
 });
